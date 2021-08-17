5e0a09deaf3932c14edcc270cd4ccb364dac3fdb
Ashutosh Ranjan <ashutosh10x@gmail.com>
2021-08-17 22:15:14 +0530

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -11,7 +11,7 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/login" component={Login} />
           <Route exact path="/home" component={Home} />
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Login} />
         </Switch>
       </BrowserRouter>
     );
